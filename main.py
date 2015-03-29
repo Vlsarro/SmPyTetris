dd607e6af4f52a70466e8362bfefba153d9e55f1
Vlsarro <Vlsarro@users.noreply.github.com>
2015-03-29 21:59:18 +0300

    Finished everything. Need to fix some bugs now.

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Tetris(QtGui.QMainWindow):
         self.setWindowTitle('Tetris Clone')
         self.show()
 
-    def cetner(self):
+    def center(self):
 
         screen = QtGui.QDesktopWidget().screenGeometry()
         size = self.geometry()
@@ -288,22 +288,22 @@ class Board(QtGui.QFrame):
 
         colorTable = [0x000000, 0xCC6666, 0x66CC66, 0x6666CC,
                       0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00]
-        
-        color = QtGui.Qcolor(colorTable[shape])
+
+        color = QtGui.QColor(colorTable[shape])
         painter.fillRect(x + 1, y + 1, self.squareWidth() - 2,
                 self. squareHeight() - 2, color)
-        
+
         painter.setPen(color.light())
         painter.drawLine(x, y + self.squareHeight() - 1, x, y)
         painter.drawLine(x, y, x + self.squareWidth() - 1, y)
-        
+
         painter.setPen(color.dark())
         painter.drawLine(x + 1, y + self.squareHeight() - 1,
                 x + self.squareWidth() - 1, y + self.squareHeight() - 1)
         painter.drawLine(x + self.squareWidth() - 1,
                 y + self.squareHeight() - 1, x + self.squareWidth() - 1, y + 1)
-        
-        
+
+
 class Tetrominoe(object):
 
     NoShape = 0
@@ -318,8 +318,113 @@ class Tetrominoe(object):
 
 class Shape(object):
 
-    pass
+    coordsTable = (
+        ((0, 0),     (0, 0),     (0, 0),     (0, 0)),
+        ((0, -1),    (0, 0),     (-1, 0),    (-1, 1)),
+        ((0, -1),    (0, 0),     (1, 0),     (1, 1)),
+        ((0, -1),    (0, 0),     (0, 1),     (0, 2)),
+        ((-1, 0),    (0, 0),     (1, 0),     (0, 1)),
+        ((0, 0),     (1, 0),     (0, 1),     (1, 1)),
+        ((-1, -1),   (0, -1),    (0, 0),     (0, 1)),
+        ((1, -1),    (0, -1),    (0, 0),     (0, 1))
+    )
 
+    def __init__(self):
+
+        self.coords = [[0,0] for i in range(4)]
+        self.pieceShape = Tetrominoe.NoShape
+
+        self.setShape(Tetrominoe.NoShape)
+
+    def shape(self):
+        return self.pieceShape
+
+    def setShape(self, shape):
+
+        table = Shape.coordsTable[shape]
+
+        for i in range(4):
+            for j in range(2):
+                self.coords[i][j] = table[i][j]
+
+        self.pieceShape = shape
+
+    def setRandomShape(self):
+        self.setShape(random.randint(1, 7))
+
+    def x(self, index):
+        return self.coords[index][0]
+
+    def y(self, index):
+        return self.coords[index][1]
+
+    def setX(self, index, x):
+        self.coords[index][0] = x
+
+    def setY(self, index, y):
+        self.coords[index][1] = y
+
+    def minX(self):
+        m = self.coords[0][0]
+        for i in range(4):
+            m = min(m, self.coords[i][0])
+
+        return m
+
+    def maxX(self):
+
+        m = self.coords[0][0]
+        for i in range(4):
+            m = max(m, self.coords[i][0])
+
+        return m
+
+    def minY(self):
+
+        m = self.coords[0][1]
+        for i in range(4):
+            m = min(m, self.coords[i][1])
+
+        return m
+
+
+    def maxY(self):
+
+        m = self.coords[0][1]
+        for i in range(4):
+            m = max(m, self.coords[i][1])
+
+        return m
+
+    def rotateLeft(self):
+
+        if self.pieceShape == Tetrominoe.SquareShape:
+            return self
+
+        result = Shape()
+        result.pieceShape = self.pieceShape
+
+        for i in range(4):
+
+            result.setX(i, self.y(i))
+            result.setY(i, -self.x(i))
+
+        return result
+
+    def rotateRight(self):
+
+        if self.pieceShape == Tetrominoe.SquareShape:
+            return self
+
+        result = Shape()
+        result.pieceShape = self.pieceShape
+
+        for i in range(4):
+
+            result.setX(i, -self.y(i))
+            result.setY(i, self.x(i))
+
+        return result
 
 def main():
 
